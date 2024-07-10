d64b81c37883a4f4a0f4f64f6773d564caea560b
almashruf <tonoy5300@gmail.com>
2024-07-10 13:47:21 +0600

    till quiz

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@
 import "../styles/App.css";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
     <Layout>
-      <Home/>
+      {/* <Home/> */}
+      {/* <Signup/> */}
+      <Quiz/>
     </Layout>
   );
 }
